48b04f44173d4a39ca9a62ba4772c80ecab85529
N1ler4 <bahtiyorov.nozim@gmail.com>
2024-01-18 23:59:00 +0500

    Homework

---
+++ script.js
@@ -0,0 +1,89 @@
+// function number(n){
+//     console.log(n**3);
+// }
+
+// number(3);
+
+// function sum(a,b){
+//     S = a + b;
+//     return S;
+// }
+// let result =  sum(3,2);
+// console.log(result);
+
+// function sum(a,b){
+//     S = a - b;
+//     return S;
+// }
+// let result =  sum(3,2);
+// console.log(result);
+
+
+// function number(a,n){
+
+//     let kv = a**n;
+//     if(n > 1 && n < 5){
+//         console.log(kv);
+//     }else{
+//         console.log("Error");
+//     }
+//     return kv;
+    
+// }
+// number(4 , 2)
+
+// function son(a){
+
+//     if(a % 2 == 1){
+//         return "Toq"
+//     }else{
+//         return "error"
+//     }
+// }
+
+// let result = son();
+// console.log(result);
+
+// function son(a){
+
+//     if(a % 2 == 0){
+//         return "Juft"
+//     }else{
+//         return "error"
+//     }
+// }
+
+// let result = son(5);
+// console.log(result);
+
+// function inr(a){
+//     for(i = 1; i <= a ;i++)
+//     console.log("A: " + i);
+// }  
+// function dec(b){
+//     for(i = 10; i >= b ;i--)
+//     console.log("B: " + i);
+// }
+// let inrResult = inr(7);
+// let decResult = dec(1);
+
+
+// function del(a){
+//     for(i = 1; i <= a ; i++){
+//         if(a % i == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+// del(21)
+
+
+// function kara(a ,b){
+//     for(i = 1 ; i <= a ; i++){
+//         for(j = 1; j <= b ; j++){
+//             console.log(i + " x " +j + " = " , i*j);
+//         }
+//     }
+// }
+// let result = kara(9,9)
+// console.log(result);
